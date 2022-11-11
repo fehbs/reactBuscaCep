b33094c703d027900536d75cbf1f1962ef71e37e
Fernando HBS <fernandobailao@gmail.com>
2022-11-11 16:04:28 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 //  http://viacep.com.br/ws/    /*base url*/
 //  01310930/json/    /*rota*/
 // http://viacep.com.br/ws/01310930/json/
-// https://viacep.com.br/ws/${cep}/json
+
 
 const api = axios.create({
-  baseURL: `https://viacep.com.br/ws/json`
+  baseURL: `https://viacep.com.br/ws`
 });
 
 export default api;
